fa1174ceaaac70615f95a093cbb2ff2c96347a87
JJimLee <56209231+JJimLee@users.noreply.github.com>
2021-05-20 01:47:23 -0700

    Convert DetailActivity to WordListFragment + Jetpack Navigation Component
    Hopefully you enjoyed getting the chance to migrate DetailActivity to WordListFragment. This is almost identical to migrating MainActivity to LetterListFragment. If you got stuck at any point, the steps are summarized below.

--- app/src/main/java/com/example/wordsapp/WordListFragment.kt
+++ app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -1,43 +1,48 @@
 package com.example.wordsapp
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.wordsapp.databinding.ActivityDetailBinding
+import com.example.wordsapp.databinding.FragmentWordListBinding
 
 class WordListFragment : Fragment() {
-    private val _binding  = null
+    private var _binding: FragmentWordListBinding? = null
+    private val binding get() = _binding!!//You then create a get-only variable so that you can reference views without having to use ?.
+
     companion object {
         const val LETTER = "letter"
         const val SEARCH_PREFIX = "https://www.google.com/search?q="
     }
-    fun get_binding(): Nothing? {
-        return _binding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentWordListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = WordAdapter(activity?.intent?.extras?.getString(LETTER).toString(), requireContext())
+
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+        )
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Retrieve a binding object that allows you to refer to views by id name
-        // Names are converted from snake case to camel case.
-        // For example, a View with the id word_one is referenced as binding.wordOne
         val binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Retrieve the LETTER from the Intent extras
-        // intent.extras.getString returns String? (String or null)
-        // so toString() guarantees that the value will be a String
-        val letterId = intent?.extras?.getString(LETTER).toString()
-
-        val recyclerView = binding.recyclerView
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = WordAdapter(letterId, this)
-
-        // Adds a [DividerItemDecoration] between items
-        recyclerView.addItemDecoration(
-            DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
-        )
-
-        title = getString(R.string.detail_prefix) + " " + letterId
     }
 }
